522507b0b2319d634f051c13fd3c25b85046d556
Simon Kesteloot <simon@kesteloot.me>
2014-01-28 18:23:35 +0100

    finish Compute Engine selector

--- application/app/scripts/controllers/main.js
+++ application/app/scripts/controllers/main.js
@@ -1,14 +1,24 @@
 'use strict';
 
 angular.module('app')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  })
-	.controller('ChooseComputeEngineCtrl', ['$scope', 'CEHelloWorld', function ($scope, $CEHelloWorld) {
-		$scope.ListComputeEngine = [];
-		$scope.ListComputeEngine.push($CEHelloWorld);
+	.controller('MainCtrl', function ($scope) {
+		$scope.awesomeThings = [
+			'HTML5 Boilerplate',
+			'AngularJS',
+			'Karma'
+		];
+	})
+	.controller('ChooseComputeEngineCtrl', ['$scope', '$log', 'ListeComputeEngine', function ($scope, $log, ListeCE) {
+		$scope.listComputeEngine = [];
+
+		$log.debug('number item ListeCE : '+ListeCE.getLength());
+
+		for (var i = ListeCE.getLength() - 1; i >= 0; i--) {
+			$scope.listComputeEngine.push(ListeCE.getItem(i));
+		}
+
+		$scope.change = function(){
+			ListeCE.selected = $scope.selected;
+			$log.debug(ListeCE.selected);
+		};
 	}]);
---
+++ application/app/scripts/service/ListeCE.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var ListeComputeEngine = function($log) {
+	var liste = [];
+
+	this.$log = $log;
+	this.liste = liste;
+
+	for (var i = arguments.length - 1; i >= 1; i--) {
+		this.liste.push(arguments[i]);
+		$log.debug('add CE to list : '+arguments[i].getName());
+	}
+};
+
+/// selected Compute Engine
+ListeComputeEngine.prototype.selected = null;
+
+/// retrieve one or all item in the list
+ListeComputeEngine.prototype.getItem = function(i) {
+	if(arguments.length===0){
+		return this.liste;
+	}
+	else if(arguments.length===1){
+		return this.liste[i];
+	}
+	else{
+		throw 'bad number of parameters';
+	}
+};
+
+ListeComputeEngine.prototype.getLength = function() {
+	return this.liste.length;
+};
+
+angular.module('app')
+	.service('ListeComputeEngine', [
+		'$log',
+		'CEHelloWorld',
+		// ^-- add new Compute Engine here --^
+		ListeComputeEngine
+	]);
